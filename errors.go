379eb8884a79c1504fc345bee494a267e94f8718
Insidious Fiddler <me@codycody31.dev>
2025-08-11 08:43:12 -0400

    refactor: remove unused funcs

--- errors.go
+++ errors.go
@@ -1,23 +1,5 @@
 package gobullmq
 
-import "fmt"
-
 var (
 	RateLimitError = "bullmq:rateLimitExceeded"
 )
-
-type StandardError struct {
-	OriginalError error
-	Message       string
-}
-
-func (e *StandardError) Error() string {
-	return fmt.Sprintf("%s: %v", e.Message, e.OriginalError)
-}
-
-func wrapError(err error, message string) error {
-	return &StandardError{
-		OriginalError: err,
-		Message:       message,
-	}
-}
